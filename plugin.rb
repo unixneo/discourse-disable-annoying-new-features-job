592584630cc29e50f7e20894e0e53e7b6947bc04
unixneo <neo@unix.com>
2021-02-04 18:34:36 +0700

    DEV: Update.

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-disable-annoying-new-features-job
 # about: plugin to disable annoying job
-# version: 0.0.5
+# version: 0.0.6
 # date: 4 January 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-disable-annoying-new-features-job
@@ -22,6 +22,14 @@ after_initialize do
       end
     end
 
+  end
+
+  module DiscourseUpdates
+    class_eval do
+      def perform_new_feature_check
+        puts "Annoying CheckNewFeatures Method disabled by plugin"
+      end  
+    end
   end
 end
 
