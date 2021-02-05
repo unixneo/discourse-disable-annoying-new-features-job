8b04c17163ad826837098f6259260d81d47f0899
unixneo <neo@unix.com>
2021-02-05 12:36:17 +0700

    DEV:   Update.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # discourse-disable-annoying-new-features-job
 
-#### Status
+#### What Does this Plugin Go
 
-Not completely working.   In the meantime, use simple CSS in themes:
+This simple Discourse plugin marks the "new features" notice in the admin dashboard, *which I find very annoying*, as "seen" so Discourse self-hosting admins do not have to see this annoying notice and dismiss the notice over and over again (especially after we rebuild a container).
+
+Discourse admins can also hide this, as below, but I prefer do use this plugin to mark the new features as "seen" and insure this code is not displayed in the DOM.
 
 ```css
 div.dashboard-new-features {
--- plugin.rb
+++ plugin.rb
@@ -2,8 +2,8 @@
 
 # name: discourse-disable-annoying-new-features-job
 # about: plugin to disable annoying job
-# version: 0.0.7
-# date: 4 January 2020
+# version: 0.1
+# date: 5 January 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-disable-annoying-new-features-job
 
@@ -17,35 +17,5 @@ after_initialize do
       DiscourseUpdates.mark_new_features_as_seen(current_user.id)
     end
   end
-
-
-  
-  # DiscourseUpdates.mark_new_features_as_seen(current_user.id)
-  # module Jobs
-
-  #   class CheckNewFeatures < ::Jobs::Scheduled
-  #     class_eval do
-  #       every 1.year
-
-  #       def execute(args)
-  #         puts "Annoying CheckNewFeatures Job disabled by plugin"
-  #       end
-  #     end
-  #   end
-
-  # end
-
-  # module DiscourseUpdates
-  #   class_eval do
-  #     def perform_new_feature_check
-  #       puts "Annoying CheckNewFeatures perform_new_feature_check method disabled by plugin"
-  #     end  
-
-  #     def unseen_new_features(user_id)
-  #       DiscourseUpdates.mark_new_features_as_seen(user_id)
-  #       return nil
-  #     end
-  #   end
-  # end
 end
 
