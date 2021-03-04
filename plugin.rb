725e030b3fde6e52c8b6b646927d92f38842ebca
unixneo <neo@unix.com>
2021-03-04 10:02:39 +0700

    REFACTOR:  Add enable/disable site setting.

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-disable-annoying-new-features-notice
 # about: plugin to disable annoying new features notice in admin dashboard
-# version: 0.121
+# version: 0.122
 # date: 4 March 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-disable-annoying-new-features-notice.git
@@ -12,15 +12,17 @@ PLUGIN_NAME = "discourse-disable-annoying-new-features-notice"
 enabled_site_setting :enable_disable_annoying_new_features_notice
 
 after_initialize do
-  Admin::DashboardController.class_eval do
-    before_action :mark_annoying_new_features_as_seen
+  if SiteSetting.enable_disable_annoying_new_features_notice?
+    Admin::DashboardController.class_eval do
+      before_action :mark_annoying_new_features_as_seen
 
-    
-    def new_features;end
+      
+      def new_features;end
 
-    private 
-    def mark_annoying_new_features_as_seen
-      DiscourseUpdates.mark_new_features_as_seen(current_user.id)
+      private 
+      def mark_annoying_new_features_as_seen
+        DiscourseUpdates.mark_new_features_as_seen(current_user.id)
+      end
     end
   end
 end
