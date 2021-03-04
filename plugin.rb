e76f804932463914bcf80f4d8a54b555b10106ae
unixneo <neo@unix.com>
2021-03-04 16:25:41 +0700

    REFACTOR:  Wrap with reloadable_patch

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-disable-annoying-new-features-notice
 # about: plugin to disable annoying new features notice in admin dashboard
-# version: 0.124
+# version: 0.125
 # date: 4 March 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-disable-annoying-new-features-notice.git
@@ -12,19 +12,21 @@ PLUGIN_NAME = "discourse-disable-annoying-new-features-notice"
 enabled_site_setting :enable_disable_annoying_new_features_notice
 
 after_initialize do
-  if SiteSetting.enable_disable_annoying_new_features_notice?
-    Admin::DashboardController.class_eval do
-      if SiteSetting.disable_new_features_dismissable_notice?
-        before_action :mark_annoying_new_features_as_seen
-      end
+  reloadable_patch do
+    if SiteSetting.enable_disable_annoying_new_features_notice?
+      Admin::DashboardController.class_eval do
+        if SiteSetting.disable_new_features_dismissable_notice?
+          before_action :mark_annoying_new_features_as_seen
+        end
 
-      if SiteSetting.disable_new_features_fixed_notice?
-        def new_features;end
-      end
+        if SiteSetting.disable_new_features_fixed_notice?
+          def new_features;end
+        end
 
-      private 
-      def mark_annoying_new_features_as_seen
-        DiscourseUpdates.mark_new_features_as_seen(current_user.id)
+        private 
+        def mark_annoying_new_features_as_seen
+          DiscourseUpdates.mark_new_features_as_seen(current_user.id)
+        end
       end
     end
   end
