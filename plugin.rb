d7e6bd7135d4c2784568fedbd3f5c9b48dae9822
unixneo <neo@unix.com>
2021-03-04 17:04:47 +0700

    REFACTOR:  Use Reloader callback.

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-disable-annoying-new-features-notice
 # about: plugin to disable annoying new features notice in admin dashboard
-# version: 0.129
+# version: 0.130
 # date: 4 March 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-disable-annoying-new-features-notice.git
@@ -12,7 +12,7 @@ PLUGIN_NAME = "discourse-disable-annoying-new-features-notice"
 enabled_site_setting :enable_disable_annoying_new_features_notice
 
 after_initialize do
-  reloadable_patch do
+  ActiveSupport::Reloader.to_prepare do
     if SiteSetting.enable_disable_annoying_new_features_notice?
       Admin::DashboardController.class_eval do
         if SiteSetting.disable_new_features_dismissable_notice?
