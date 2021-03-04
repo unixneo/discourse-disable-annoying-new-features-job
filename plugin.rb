0c023b33a4f2ae83ef4c09202a185d41e2715164
unixneo <neo@unix.com>
2021-03-04 17:24:08 +0700

    TEST: Comment out callback.

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-disable-annoying-new-features-notice
 # about: plugin to disable annoying new features notice in admin dashboard
-# version: 0.131
+# version: 0.132
 # date: 4 March 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-disable-annoying-new-features-notice.git
@@ -11,7 +11,7 @@ PLUGIN_NAME = "discourse-disable-annoying-new-features-notice"
 
 enabled_site_setting :enable_disable_annoying_new_features_notice
 
-after_initialize do
+#after_initialize do
   if SiteSetting.enable_disable_annoying_new_features_notice?
     Admin::DashboardController.class_eval do
       if SiteSetting.disable_new_features_dismissable_notice?
@@ -35,5 +35,5 @@ after_initialize do
       end
     end
   end
-end
+#end
 
