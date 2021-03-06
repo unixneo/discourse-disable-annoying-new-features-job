79c4c10bb027d4b1aaa85b91251124d0b50bc91d
unixneo <neo@unix.com>
2021-03-06 07:14:12 +0700

    REFACTOR: New plugin name.

--- plugin.rb
+++ plugin.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
-# name: discourse-disable-annoying-new-features-notice
-# about: plugin to disable annoying new features notice in admin dashboard
-# version: 0.138
-# date: 5 March 2020
+# name: discourse-disable-new-features-notice
+# about: Disables new features notices in admin dashboard
+# version: 0.139
+# date: 6 March 2020
 # authors: Neo
-# url: https://github.com/unixneo/discourse-disable-annoying-new-features-notice.git
+# url: https://github.com/unixneo/discourse-disable-new-features-notice.git
 
-PLUGIN_NAME = "discourse-disable-annoying-new-features-notice"
+PLUGIN_NAME = "discourse-disable-new-features-notice"
 
-enabled_site_setting :enable_disable_annoying_new_features_notice
+enabled_site_setting :enable_disable_new_features_notice
 
 after_initialize do
   Admin::DashboardController.class_eval do
-    before_action :mark_annoying_new_features_as_seen
+    before_action :mark_new_features_as_seen
 
     def new_features
       new_features = DiscourseUpdates.new_features
       has_unseen_features = DiscourseUpdates.has_unseen_features?(current_user.id)
-      if SiteSetting.enable_disable_annoying_new_features_notice? && SiteSetting.disable_new_features_fixed_notice?
-          new_features = nil
-          has_unseen_features = false
+      if SiteSetting.enable_disable_new_features_notice? && SiteSetting.disable_new_features_fixed_notice?
+        new_features = nil
+        has_unseen_features = false
       end
       data = {
         new_features: new_features,
@@ -31,8 +31,8 @@ after_initialize do
     end
 
     private 
-    def mark_annoying_new_features_as_seen
-      if SiteSetting.enable_disable_annoying_new_features_notice? && SiteSetting.disable_new_features_dismissable_notice?
+    def mark_new_features_as_seen
+      if SiteSetting.enable_disable_new_features_notice? && SiteSetting.disable_new_features_dismissable_notice?
         DiscourseUpdates.mark_new_features_as_seen(current_user.id)
       end
     end
